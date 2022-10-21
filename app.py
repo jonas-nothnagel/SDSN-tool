0d8c065dc23b062377fa35c89a8195604e9d5553
prashant <prashant.singh@giz.de>
2022-10-21 05:39:23 +0200

    ver0.2 app update

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import appStore.keyword_search as keyword_search
 import appStore.sdg_analysis as sdg_analysis
-import appStore.coherence as coherence
+#import appStore.coherence as coherence
 import appStore.info as info
 from appStore.multiapp import MultiApp
 import streamlit as st
 
-# This branch is before the download option was implemented
-st.set_page_config(f'SDSN x GIZ Policy Action Tracking v0.1', layout="wide")
+st.set_page_config(page_title = 'SDSN x GIZ Policy Action Tracking v0.2', 
+                   initial_sidebar_state='expanded', layout="wide") 
 
 app = MultiApp()
 
-app.add_app("SDG Analysis", sdg_analysis.app)
-app.add_app("Search", keyword_search.app)
-app.add_app("NDC Coherence", coherence.app)
-app.add_app("About", info.app)
+app.add_app("About","house", info.app)
+app.add_app("SDG Analysis","gear",sdg_analysis.app)
+app.add_app("Search","search", keyword_search.app)
 
 app.run()
