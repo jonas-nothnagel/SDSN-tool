593bc97565b65c65cb876874153cc88ee335e12b
prashant <prashant.singh@giz.de>
2022-10-21 06:43:24 +0200

    reverting to table

--- appStore/sdg_analysis.py
+++ appStore/sdg_analysis.py
@@ -113,7 +113,7 @@ def app():
                     
                 c7, c8, c9 = st.columns([1, 10, 1])
                 with c8:
-                    st.dataframe(df)
+                    st.table(df)
 
 
 #     1. Keyword heatmap \n
