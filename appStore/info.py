7d9a9811540f16a250461673f97a97b895dae9fc
Prashant Singh <ppsingh@users.noreply.huggingface.co>
2022-11-07 08:34:49 +0000

    Update appStore/info.py

--- appStore/info.py
+++ appStore/info.py
@@ -14,7 +14,8 @@ def app():
     footer = """
            <div class="footer-custom">
                Guidance & Feedback - <a href="https://www.linkedin.com/in/maren-bernlöhr-149891222" target="_blank">Maren Bernlöhr</a> |
-               <a href="https://www.linkedin.com/in/manuelkuhm" target="_blank">Manuel Kuhm</a> 
+               <a href="https://www.linkedin.com/in/manuelkuhm" target="_blank">Manuel Kuhm</a> |
+               
                Developer - <a href="https://www.linkedin.com/in/erik-lehmann-giz/" target="_blank">Erik Lehmann</a>  |   
                <a href="https://www.linkedin.com/in/jonas-nothnagel-bb42b114b/" target="_blank">Jonas Nothnagel</a>   |
                <a href="https://www.linkedin.com/in/prashantpsingh/" target="_blank">Prashant Singh</a> |
@@ -41,7 +42,7 @@ The collaboration aims to determine the potential of NLP methods for tracking po
     <br>
     """
     st.markdown(intro, unsafe_allow_html=True)
-    st.image("appStore/img/pic1.png", caption="NDC Coherence")
+    st.image("appStore/img/pic1.png", caption="")
     #st.subheader("Methodology")
     #st.write("Each sentence in the generated answer ends with a coloured tooltip; the colour ranges from red to green. "
     #         "The tooltip contains a value representing answer sentence similarity to a specific sentence in the "
